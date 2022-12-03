24e325980234088e24a8b4b26db7268ba30f7f1a
Haitao Huang <v0rt3xh@gmail.com>
2022-12-02 22:51:12 -0600

    Try to compute number of neighbors: debug

--- src/cpp/src/pipeline/trainer.cpp
+++ src/cpp/src/pipeline/trainer.cpp
@@ -104,6 +104,7 @@ void SynchronousTrainer::train(int num_epochs) {
     {
         // gets data and parameters for the next batch
         shared_ptr<Batch> batch = dataloader_->getBatch();
+        batch->dense_graph_.performMap();
         Indices outNeighbors = batch->dense_graph_.getNumNeighbors(false);
         // float outNeighborsSum = torch::sum(outNeighbors).item<float>();
         SPDLOG_INFO("HAAHAHAH {} ", outNeighbors.sizes()[0]);
